96d16dbed0fd8b302ce13dd7522240e33e31817d
tcwelch <43589658+tcwelch@users.noreply.github.com>
2021-11-12 14:22:24 -0800

    kernel fitting done
    linear, polynomial, gaussian, laplace

--- cross_validation.m
+++ cross_validation.m
@@ -1,10 +1,11 @@
 %This function can be called on y and y_pred for [zeta, wn] as rows or with
 %just zeta or just wn labels and predictions.
-function [overall_rmse] = cross_validation(y1,y2,y3,y4,y5,y_pred1,y_pred2,y_pred3,y_pred4,y_pred5)
-    rmse1 = rms(reshape(y1,[],1) - reshape(y_pred1,[],1));
-    rmse2 = rms(reshape(y2,[],1) - reshape(y_pred2,[],1));
-    rmse3 = rms(reshape(y3,[],1) - reshape(y_pred3,[],1));
-    rmse4 = rms(reshape(y4,[],1) - reshape(y_pred4,[],1));
-    rmse5 = rms(reshape(y5,[],1) - reshape(y_pred5,[],1));
-    overall_rmse = mean([rmse1,rmse2,rmse3,rmse4,rmse5]);
+function [overall_prmse] = cross_validation(y1,y2,y3,y4,y5,y_pred1,y_pred2,y_pred3,y_pred4,y_pred5)
+    N = length(reshape(y_pred1,[],1));
+    prmse1 = rms(reshape(y1,[],1) - reshape(y_pred1,[],1))*(100*N/sum(reshape(y1,[],1)));
+    prmse2 = rms(reshape(y2,[],1) - reshape(y_pred2,[],1))*(100*N/sum(reshape(y2,[],1)));
+    prmse3 = rms(reshape(y3,[],1) - reshape(y_pred3,[],1))*(100*N/sum(reshape(y3,[],1)));
+    prmse4 = rms(reshape(y4,[],1) - reshape(y_pred4,[],1))*(100*N/sum(reshape(y4,[],1)));
+    prmse5 = rms(reshape(y5,[],1) - reshape(y_pred5,[],1))*(100*N/sum(reshape(y5,[],1)));
+    overall_prmse = mean([prmse1,prmse2,prmse3,prmse4,prmse5]);
 end
