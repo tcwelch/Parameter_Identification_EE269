4d2a71fdfaa8f1108b1fb553a317fb1b552c1bd8
James P <38028854+JamesPillot@users.noreply.github.com>
2021-11-19 14:55:44 -0800

    FFT Feature Selection
    Took FFT and ran knn on that, performed slightly worse than just taking the data straight up

--- knn_sysID.m
+++ knn_sysID.m
@@ -41,6 +41,46 @@ end
 %% 4. No feature extraction - weighted average: Testing
 prsme_value_4 = test_evaluation(X_test,Y_test,X_valid1, X_valid2, X_valid3, X_valid4, X_valid5, ...
     Y_valid1, Y_valid2, Y_valid3, Y_valid4, Y_valid5, best_k2, use_uniform);
+%% 5. FFT - uniform average: Training
+use_uniform = 1; % if 1, use uniform average for knn regression, if 0 use weighted average
+k_max = 25; % largest number of surrounding neighbors to compute average with
+k_values = 1:1:k_max;
+prsme_values_1 = zeros(length(k_values),1);
+
+% Take FFT 
+X_valid1_fft = abs(fft(X_valid1,[],2));
+X_valid2_fft = abs(fft(X_valid2,[],2));
+X_valid3_fft = abs(fft(X_valid3,[],2));
+X_valid4_fft = abs(fft(X_valid4,[],2));
+X_valid5_fft = abs(fft(X_valid5,[],2));
+
+for k = k_values
+   prsme_values_1(k) = cross_validate(X_valid1_fft, X_valid2_fft, X_valid3_fft, X_valid4_fft, X_valid5_fft, ...
+    Y_valid1, Y_valid2, Y_valid3, Y_valid4, Y_valid5, k, use_uniform); 
+end
+
+% Identify best k value
+[~, best_k1] = min(prsme_values_1);
+%% 6. FFT - uniform average: Testing
+load('test_data.mat');
+X_test_fft = abs(fft(X_test,[],2));
+prsme_value_2 = test_evaluation(X_test_fft,Y_test,X_valid1_fft, X_valid2_fft, X_valid3_fft, X_valid4_fft, X_valid5_fft, ...
+    Y_valid1, Y_valid2, Y_valid3, Y_valid4, Y_valid5, k, use_uniform); 
+%% 7. FFT - weighted average: Training
+use_uniform = 0; % if 1, use uniform average for knn regression, if 0 use weighted average
+k_max = 25; % largest number of surrounding neighbors to compute average with
+k_values = 1:1:k_max;
+prsme_values_3 = zeros(length(k_values),1);
+
+for k = k_values
+   prsme_values_3(k) = cross_validate(X_valid1_fft, X_valid2_fft, X_valid3_fft, X_valid4_fft, X_valid5_fft, ...
+    Y_valid1, Y_valid2, Y_valid3, Y_valid4, Y_valid5, k, use_uniform); 
+end
+
+[~, best_k2] = min(prsme_values_3);
+%% 8. FFT - weighted average: Testing
+prsme_value_4 = test_evaluation(X_test_fft,Y_test,X_valid1_fft, X_valid2_fft, X_valid3_fft, X_valid4_fft, X_valid5_fft, ...
+    Y_valid1, Y_valid2, Y_valid3, Y_valid4, Y_valid5, k, use_uniform); 
 %% Functions
 function prmse = calc_percent_rmse(x_actual,x_corrupted)
     N = length(x_actual);
